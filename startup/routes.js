dcbadcad92d8b877f7349c444651a9dea2244cf5
swathinakshatra <swathinakshatra485@gmail.com>
2023-05-01 12:19:03 +0530

    initial

--- startup/routes.js
+++ startup/routes.js
@@ -5,15 +5,14 @@ const admin = require('../routes/Admin');
 const images = require('../routes/images');
 const addposts=require('../routes/addposts');
 
-app.get('/', async (req, res, next) => {
-    
-      return res.status(200).send('crazy offers home route');
-    
-  });
+module.exports=function(app){
+    app.get('/', async (req, res) => {
+        return res.status(200).send('Crazy offers Home route');
+      });
 app.use(express.json());
 app.use('/api/addposts',addposts);
 app.use('/api/users',users);
 app.use('/api/categories',categories);
 app.use('/api/admin', admin);
 app.use('/api/images', images);
-
+}
